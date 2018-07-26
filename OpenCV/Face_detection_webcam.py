e4adea30f77d9f4344b0858e2cea9b4777cdd08c
chandareshkumar R <chandareshkumar@gmail.com>
2018-07-27 00:19:52 +0530

    Update Face_detection_webcam.py
    Added face count

--- OpenCV/Face_detection_webcam.py
+++ OpenCV/Face_detection_webcam.py
@@ -1,31 +1,34 @@
 
-# coding: utf-8
 
-# In[1]:
 
 
 import numpy as np
 import cv2
 
 
-# In[51]:
+
 
 
 face_cascade = cv2.CascadeClassifier('/Users/chand/Downloads/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/Users/chand/Downloads/haarcascade_eye.xml')
 
-
-# In[55]:
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 cap = cv2.VideoCapture(0) 
 while(cap.isOpened()):
 	ret, frame = cap.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	gray = cv2.flip(gray,90)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	face=len(faces)
+
+     
 	for (x,y,w,h) in faces:
+	    #print(len(faces))
+     
 	    cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+	    cv2.putText(gray, str(face),(900,160),font,4,(0,0,0),4,cv2.LINE_AA)   # displaying the count     
 	    roi_gray = gray[y:y+h, x:x+w]
 
 	    eyes = eye_cascade.detectMultiScale(roi_gray)
@@ -40,13 +43,6 @@ while(cap.isOpened()):
 	cv2.imshow('frame',gray)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 	    break
-cap.release()
-cv2.destroyAllWindows()
-
-
-# In[53]:
-
-
 cap.release()
 cv2.destroyAllWindows()
 
